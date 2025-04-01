b6fb6a6f496e90c1fcd126cc2f08cbc181ba2631
abl00mfield <amanda.bloomfield@gmail.com>
2025-04-01 11:52:30 -0600

    adds service mode for hoot service

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,31 @@
-import { useContext } from 'react';
-import { Routes, Route } from 'react-router';
+import { useContext } from "react";
+import { Routes, Route } from "react-router";
 
-import NavBar from './components/NavBar/NavBar';
-import SignUpForm from './components/SignUpForm/SignUpForm';
-import SignInForm from './components/SignInForm/SignInForm';
-import Landing from './components/Landing/Landing';
-import Dashboard from './components/Dashboard/Dashboard';
+import NavBar from "./components/NavBar/NavBar";
+import SignUpForm from "./components/SignUpForm/SignUpForm";
+import SignInForm from "./components/SignInForm/SignInForm";
+import Landing from "./components/Landing/Landing";
+import Dashboard from "./components/Dashboard/Dashboard";
+import HootList from "./components/HootList/HootList";
 
-import { UserContext } from './contexts/UserContext';
+import { UserContext } from "./contexts/UserContext";
 
 const App = () => {
   const { user } = useContext(UserContext);
-  
+
   return (
     <>
-      <NavBar/>
+      <NavBar />
       <Routes>
-        <Route path='/' element={user ? <Dashboard /> : <Landing />} />
-        <Route path='/sign-up' element={<SignUpForm />} />
-        <Route path='/sign-in' element={<SignInForm />} />
+        <Route path="/" element={user ? <Dashboard /> : <Landing />} />
+        {user ? (
+          <Route path="/hoots" element={<HootList />} />
+        ) : (
+          <>
+            <Route path="/sign-up" element={<SignUpForm />} />
+            <Route path="/sign-in" element={<SignInForm />} />
+          </>
+        )}
       </Routes>
     </>
   );
